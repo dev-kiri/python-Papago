501e2a3054e6bab3c688bfaa0d6b0bb50775fe2c
dev-kiri <72954146+dev-kiri@users.noreply.github.com>
2021-04-09 16:35:55 +0900

    fix 403 error

--- src/papago.py
+++ src/papago.py
@@ -3,89 +3,102 @@ import hmac
 import base64
 import time
 import uuid
+import asyncio
+
+from typing import Dict
 
 class Papago:
-
-    def __init__(self, source, target, text):
-        if not isinstance(source, str): raise TypeError('Invalid source parameter type')
-        if not isinstance(target, str): raise TypeError('Invalid target parameter type')
-        if not isinstance(text, str): raise TypeError('Invalid text parameter type')
+    def __init__(self, source: str, target: str):
         self.source = source
         self.target = target
-        self.text = text
 
-    def __encrypt(self, text, passphrase):
-        hash = hmac.digest(passphrase.encode(), text.encode(), 'MD5')
-        return base64.b64encode(hash).decode()
+    def __getHash(self, message: bytearray, key: bytearray) -> str:
+        md5 = hmac.digest(key, message, 'MD5')
+        return base64.b64encode(md5).decode()
 
-    async def __detect(self, query):
+    def __generateHeaders(self, url: str, filter: list) -> Dict[str, str]:
+        auth_key = b'v1.5.6_97f6918302'
+        timestamp = str(int(time.time() * 1000))
+        deviceId = str(uuid.uuid4())
+        hashstr = self.__getHash(f'{deviceId}\n{url}\n{timestamp}'.encode(), auth_key)
+        data = {
+            'authorization': f'PPG {deviceId}:{hashstr}',
+            'deviceId': deviceId,
+            'timestamp': timestamp,
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'device-type': 'pc',
+            'referer': 'https://papago.naver.com/',
+            'x-apigw-partnerid': 'papago',
+            'user-agent': 'Mozilla/5.0'
+        }
+        return {k: v for k, v in data.items() if k in filter}
+
+    def __check_status(self, response: requests.Response) -> bool:
+        status = response.status_code
+        if status == 500:
+            raise Exception('500: Invalid parameter')
+        if 400 <= status < 500:
+            raise Exception(f'{status}: request aborted')
+        if status == 200:
+            return True
+
+    async def __detect(self, query: str) -> str:
         url = 'https://papago.naver.com/apis/langs/dect'
-        timestamp = str(int(time.time() * 1000))
-        deviceId = uuid.uuid4()
-        hash = self.__encrypt(f'{deviceId}\n{url}\n{timestamp}', 'v1.5.2_0d13cb6cf4')
-        auth = f'PPG {deviceId}:{hash}'
-        response = requests.post(
-            url,
-            headers = {
-                'Authorization': auth,
-                'Timestamp': timestamp,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
-            },
-            data = dict(query=query)
-        ).json()
-        langcode = response['langCode']
+        headers = self.__generateHeaders(url, [
+            'content-type',
+            'authorization',
+            'timestamp',
+            'user-agent',
+            'device-type',
+            'referer'
+        ])
+        response = requests.post(url, headers=headers, data=dict(query=query))
+        if self.__check_status(response):
+            content = response.json()
+            lang = content['langCode']
+            if lang != 'unk':
+                return lang
 
-        if langcode != 'unk':
-            return langcode
-
-    async def translate(self, honorific = False, verbose = False):
+    async def translate(self, text: str, honorific: bool = False, verbose: bool = False) -> Dict[str, str]:
         url = 'https://papago.naver.com/apis/n2mt/translate'
-        timestamp = str(int(time.time() * 1000))
-        deviceId = uuid.uuid4()
-        hash = self.__encrypt(f'{deviceId}\n{url}\n{timestamp}', 'v1.5.2_0d13cb6cf4')
-        auth = f'PPG {deviceId}:{hash}'
-        if self.source == 'detect':
-            self.source = await self.__detect(self.text)
-            if not self.source:
-                raise Exception('Cannot detect text')
-        response = requests.post(
-            url,
-            headers = {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Authorization': auth,
-                'Timestamp': timestamp,
-                'Device-Type': 'pc',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
-            },
-            data = {
-                'deviceId': deviceId,
-                'locale': 'ko',
-                'dict': 'true',
-                'dictDisplay': '30',
-                'honorific': 'true' if honorific else 'false',
-                'instant': 'false',
-                'paging': 'false',
-                'source': self.source,
-                'target': self.target,
-                'text': self.text
-            }
-        )
-        if response.status_code == 500: raise ValueError('Invalid langCode parameter')
-        if response.status_code == 403: raise ConnectionAbortedError('Connnection aborted')
-        if response.status_code != 200: raise Exception('Unexpected Error')
-        body = response.json()
-        if verbose: return body
-        sound = None
-        srcSound = None
-        if 'tlit' in body:
-            sound = ' '.join(list(map(lambda x: x['phoneme'], body['tlit']['message']['tlitResult'])))
-        if 'tlitSrc' in body:
-            srcSound = ' '.join(list(map(lambda x: x['phoneme'], body['tlitSrc']['message']['tlitResult'])))
-        return {
+        headers = self.__generateHeaders(url, [
+            'content-type',
+            'authorization',
+            'timestamp',
+            'user-agent',
+            'device-type',
+            'referer',
+            'x-apigw-partnerid',
+            'deviceId'
+        ])
+        data = {k: v for k, v in headers.items() if k in ['authorization', 'timestamp', 'deviceId']}
+        data = {'authroization' if k == 'authorization' else k: v for k, v in data.items()}
+        response = requests.post(url, headers=headers, data={
+            **data,
+            'locale': 'ko',
+            'dict': 'true',
+            'dictDisplay': '30',
+            'honorific': 'true' if honorific else 'false',
+            'instant': 'true',
+            'paging': 'false',
             'source': self.source,
             'target': self.target,
-            'text': self.text,
-            'translatedText': body['translatedText'],
-            'sound': sound,
-            'srcSound': srcSound
-        }
+            'text': text
+        })
+        if self.__check_status(response):
+            content = response.json()
+            if verbose: return content
+            sound: str = None
+            srcSound: str = None
+            if 'tlit' in content:
+                sound = ' '.join(list(map(lambda x: x['phoneme'], content['tlit']['message']['tlitResult'])))
+            if 'tlitSrc' in content:
+                srcSound = ' '.join(list(map(lambda x: x['phoneme'], content['tlitSrc']['message']['tlitResult'])))
+            return {
+                'source': self.source,
+                'target': self.target,
+                'text': text,
+                'translatedText': content['translatedText'],
+                'sound': sound,
+                'srcSound': srcSound
+            }
